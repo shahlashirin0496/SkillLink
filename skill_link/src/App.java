8048ca2c144e46ffa6f908ea4488c003b72481d2
SREENAND K <sreenand1257@gmail.com>
2025-08-08 11:44:04 +0530

    added frame

---
+++ skill_link/src/App.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        JFrame obj = new JFrame("skill link");
+ 
+        obj.setSize(400,400);
+        obj.setVisible(true);
+
+        
+    }
+}
